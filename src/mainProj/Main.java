50a20035130ffcf0e463d6011056e02eb818cc5d
NRosu <nicu.rosu1967@gmail.com>
2022-10-24 20:36:29 +0300

    Updated Main.java with title

--- src/mainProj/Main.java
+++ src/mainProj/Main.java
@@ -11,7 +11,7 @@ public class Main {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html
          */
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
@@ -37,6 +37,7 @@ public class Main {
                 new IntrodFrame().setVisible(true);
             }
         });
+        System.out.println("title");
     }
 
 }
